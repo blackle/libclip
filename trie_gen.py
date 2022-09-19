f7c6b8ca5d957f4ac1c5ce752383a2ff6fd0a4db
blackle <isabelle@blackle-mori.com>
2022-09-19 19:31:27 -0400

    add vocab

--- trie_gen.py
+++ trie_gen.py
@@ -8,7 +8,7 @@ import sys
 
 @lru_cache()
 def default_bpe():
-    return os.path.join(os.path.dirname(os.path.abspath(__file__)), "bpe_simple_vocab_16e6.txt")
+    return os.path.join(os.path.dirname(os.path.abspath(__file__)), "bpe_simple_vocab_small.txt")
 
 @lru_cache()
 def bytes_to_unicode():
